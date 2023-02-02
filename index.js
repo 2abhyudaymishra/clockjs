33721e6ef952ff45cc3a03acc74bfaf8273b87c7
ABHYUDAY MISHRA <parthmishra02@gmail.com>
2023-02-02 12:38:23 +0530

    function fixed

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
+window.addEventListener("load",()=>{
 
-setInterval(() => {
-    d = new Date();
-    htime= d.getHours();
-    mtime= d.getMinutes();
-    stime= d.getSeconds();
-    years=d.getFullYear();
-    date=d.getDate();
-    month=d.getMonth()+1;
-    hrotation = 30*htime + mtime/2;
-    mrotation = 6*mtime;
-    srotation = 6*stime;
-
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    min.style.transform = `rotate(${mrotation}deg)`;
-    sec.style.transform = `rotate(${srotation}deg)`;
-
-    dhour.innerHTML=htime + " <br> hours ";
-    dmin.innerHTML=mtime+ " <br> minutes ";
-    dsec.innerHTML=stime + " <br> seconds ";
-    ddate.innerHTML=date;
-    dmonth.innerHTML=month;
-    dyear.innerHTML=years;
-    
     switched.addEventListener('change',()=>{
         circle.classList.toggle('dispn');
         digital.classList.toggle('dispn');
         
     })
 
-},1000);
+    setInterval(() => {
+        d = new Date();
+        htime= d.getHours();
+        mtime= d.getMinutes();
+        stime= d.getSeconds();
+        years=d.getFullYear();
+        date=d.getDate();
+        month=d.getMonth()+1;
+        hrotation = 30*htime + mtime/2;
+        mrotation = 6*mtime;
+        srotation = 6*stime;
+    
+        hour.style.transform = `rotate(${hrotation}deg)`;
+        min.style.transform = `rotate(${mrotation}deg)`;
+        sec.style.transform = `rotate(${srotation}deg)`;
+    
+        dhour.innerHTML=htime + " <br> hours ";
+        dmin.innerHTML=mtime+ " <br> minutes ";
+        dsec.innerHTML=stime + " <br> seconds ";
+        ddate.innerHTML=date;
+        dmonth.innerHTML=month;
+        dyear.innerHTML=years;
+        
+        
+    
+    },1000);
+})
